513e3b414b10fe41925a5cafd5decb602c1b916a
HP3-16 <hp3.16.bsr@gmail.com>
2023-10-02 00:59:25 +0530

    Occurences stated

--- Recursion/first-ele-occ-rec.cpp
+++ Recursion/first-ele-occ-rec.cpp
@@ -16,9 +16,45 @@ int occ = firstOcc(arr+1,size-1,key);
     return occ+1;
 } 
 
+int lastOcc(int *arr, int size, int key){
+if(size == 0)
+    return -1;
+
+int idx = lastOcc(arr+1,size-1,key);
+if(idx==-1){
+    if(arr[idx]==key)
+        return 0;
+    else
+        return -1;
+}
+
+return idx+1;
+        
+}
+
+void allOcc(int *a,int i, int size, int k){
+    if(i==size)
+        return;
+    if(a[i] == k)
+        cout<<i<<", ";
+      allOcc(a,i+1,size,k); 
+
+}
+int linSearchRec(int *arr,int idx,int size, int key){
+if(idx==size)
+    return -1;
+if(arr[idx] == key)
+    return idx;
+int pos = linSearchRec(arr,idx+1,size,key);
+return pos;
+}
+
 int main(){
 
     int arr[] = {1,2,3,4,7,6,7};
     int size = sizeof(arr)/sizeof(int);
     cout<<firstOcc(arr,size,7);
+
+    allOcc(arr,0,size,7);
+
 }
