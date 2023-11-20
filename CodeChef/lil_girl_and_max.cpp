061299c9cfb1f3012007d5a67081326424f8515b
HP3-16 <hp3.16.bsr@gmail.com>
2023-11-20 16:06:36 +0530

    codeforces 276C

--- CodeChef/lil_girl_and_max.cpp
+++ CodeChef/lil_girl_and_max.cpp
@@ -9,9 +9,45 @@ void file_i_o(){
         freopen("output.txt","w",stdout);
     #endif
 }
+
 int main(){
-    int n,q;
-    cin>>n>>q;
+    file_i_o();
+    int n,k;
+    cin>>n>>k;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++)
+        cin>>nums[i];
+
+    vector<pair<int,int> > q(k);
+
+    for(int i=0;i<k;i++)
+        {
+            cin>>q[i].first;
+            cin>>q[i].second;
+        }
+
+
+    vector<int> v(n,0);
     
+    for(int i=0;i<k;i++){
+        if(q[i].first-1<n)
+            v[q[i].first-1]++;
+
+        if(q[i].second<n)
+            v[q[i].second]--;
+        
+
+    }
+
+    for(int i =1;i<n;i++)
+        v[i] = v[i]+v[i-1];
+
+    sort(v.begin(),v.end());
+    sort(nums.begin(),nums.end());
+    int ans = 0;
+    for(int i =0;i<n;i++)
+        ans += (v[i]*nums[i]);
+    cout<<ans;
+
     return 0;
 }
